0d492e1aa9389740b3e575289db34a9698c054b6
Tiago Pinhal <pinhalcode@gmail.com>
2025-09-24 21:42:36 -0300

    Configura a aplicação para utilizar banco de dados H2 para os testes

--- src/main/java/br/com/alura/AluraFake/user/User.java
+++ src/main/java/br/com/alura/AluraFake/user/User.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
--- src/main/resources/db/migration/V2__createTableCourse.sql
+++ src/main/resources/db/migration/V2__createTableCourse.sql
@@ -1,11 +1,14 @@
 CREATE TABLE Course (
-    id bigint(20) NOT NULL AUTO_INCREMENT,
-    createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    title varchar(50) NOT NULL,
-    description varchar(255) NOT NULL,
-    instructor_id bigint(20) NOT NULL,
-    status enum('BUILDING', 'PUBLISHED') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'BUILDING',
-    publishedAt datetime DEFAULT NULL,
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    title VARCHAR(50) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    instructor_id BIGINT NOT NULL,
+    status VARCHAR(20) NOT NULL DEFAULT 'BUILDING',
+    publishedAt TIMESTAMP DEFAULT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT FK_Author FOREIGN KEY (instructor_id) REFERENCES User(id) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
+    CONSTRAINT FK_Author FOREIGN KEY (instructor_id) REFERENCES Users(id) ON DELETE CASCADE
+);
+
+ALTER TABLE Course ADD CONSTRAINT CK_Course_Status
+CHECK (status IN ('BUILDING', 'PUBLISHED'));
